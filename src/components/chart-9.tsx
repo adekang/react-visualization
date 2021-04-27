e6a536004baf9415fd10253dc4a037ec94e7fe7a
TopLong <201497709@qq.com>
2021-04-27 20:47:14 +0800

    完成 Chart9

--- src/components/chart-9.tsx
+++ src/components/chart-9.tsx
@@ -1,14 +1,58 @@
 import React, {useEffect, useRef} from 'react';
 import * as echarts from 'echarts';
+import {createEchartsOptions} from '../shared/create-echarts-options';
+import {px} from '../shared/px';
 
 export const Chart9 = () => {
   const divRef = useRef(null);
   useEffect(() => {
     const myChart = echarts.init(divRef.current);
+    myChart.setOption(createEchartsOptions({
+      color: '#F7A110',
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: [0, 18, 28, 38, 48, 58, 68, 78],
+        splitLine: {show: true, lineStyle: {color: '#073E78'}},
+        axisTick: {show: false},
+        axisLine: {show: false},
+      },
+      yAxis: {
+        type: 'value',
+        splitLine: {lineStyle: {color: '#073E78'}},
+        axisLabel: {
+          formatter(val) {
+            return val * 100 + '%';
+          }
+        }
+      },
+      series: [{
+        data: [0.15, 0.13, 0.11,
+          0.13, 0.14, 0.15,
+          0.16, 0.18],
+        type: 'line',
+        symbol: 'circle',
+        symbolSize: px(12),
+        lineStyle: {
+          width: px(2),
+          color: '#F7A110'
+        },
+        areaStyle: {
+          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+            offset: 0,
+            color: '#F7A110'
+          }, {
+            offset: 1,
+            color: '#1B1D52'
+          }]),
+        }
+      }],
+    }));
   }, []);
 
   return (
-    <div className="年龄段-图3">9
+    <div className="年龄段-图3">
+      <h3>犯罪年龄趋势图</h3>
       <div ref={divRef} className="chart">
 
       </div>
